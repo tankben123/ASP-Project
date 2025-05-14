9ac9927433169fbba856567cdebfa54775c3285c
toannt <nguyenthetoan939393@gmail.com>
2025-05-14 16:48:00 +0700

    update source

---
+++ Part001/WebApp/Models/BrandRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApp.Models
+{
+    public class BrandRepository
+    {
+        StoreContext context;
+        public BrandRepository(StoreContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Brand> GetBrands()
+        {
+            return context.Brands.ToList();
+        }
+    }
+}
