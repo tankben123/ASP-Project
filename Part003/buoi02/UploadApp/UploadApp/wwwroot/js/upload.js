e5f1f126e75cf3a6667e8c02b088d831cd296788
ToanNguyen <nguyenthetoan939393@gmail.com>
2025-07-17 23:18:01 +0700

    fix

---
+++ Part003/buoi02/UploadApp/UploadApp/wwwroot/js/upload.js
@@ -0,0 +1,17 @@
+﻿/// <reference path = "../lib/jquery/jquery.min.js"/>
+//console.log("upload.js has loaded!");
+
+//document.querySelectorAll('script').forEach(s => {
+//    if (s.src.toLowerCase().includes("jquery")) {
+//        console.log("📌 jQuery is loaded from:", s.src);
+//    }
+//});
+
+$(document).ready(function () {
+ /*   console.log("jQuery ready in upload.js");*/
+
+    $('form[name="frm"]').submit(function (ev) {
+        ev.preventDefault();
+       /* alert("Submit bị chặn!");*/
+    });
+});
